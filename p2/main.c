4931a84a360a21fac20c3c2f42770980f883cfd9
2023123010 <seonghyeon500@kau.kr>
2026-04-20 15:24:12 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -2,32 +2,32 @@
 
 int main(void)
 {
-    int i = 0, n = 0;
+    int n = 0;
     int count_alpha = 0, count_num = 0;
     int max_alpha = 0, max_num = 0;
     char ch;
     
     scanf("%d", &n);
-    for(i=1; i<=n; i++) {
+    getchar(); // 공백 처리
+    for(int i=1; i<=n; i++) {
+        // 문자열 하나씩 읽어들이기
         scanf("%c", &ch);
-        
-        if (ch == '\n') {
-            i -= 1;
-        }
-        
-        if(ch >= 'a' && ch <= 'z') {
+
+        if (ch >= 'a' && ch <= 'z') {
             count_alpha++;
             count_num = 0;
             if (count_alpha > max_alpha) {
                 max_alpha = count_alpha;
             }
-        }
-        if(ch >= '0' && ch <= '9') {
+        } else if (ch >= '0' && ch <= '9') {
             count_num++;
             count_alpha = 0;
             if (count_num > max_num) {
                 max_num = count_num;
             }
+        } else {
+            count_alpha = 0;
+            count_num = 0;
         }
     }
     printf("%d\n", max_alpha);
